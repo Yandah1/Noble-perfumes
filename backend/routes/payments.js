e8a37186a092dd1d02810a011f91d81c6e0a7c1e
Yandah1 <nonku003@gmail.com>
2024-03-12 10:41:10 +0000

    merge confilct

--- backend/routes/payments.js
+++ backend/routes/payments.js
@@ -8,7 +8,7 @@ const { generatePayFastUrl } = require('../helpers/payment-gateways'); // Import
 router.post('/payfast', async (req, res) => {
     try {
         // Process the initial payment data
-        // Generate PayFast payment URL
+i        // Generate PayFast payment URL
         const orderId = '1234'; // Get orderId from request or generate dynamically
         const amount = '10.00'; // Get payment amount from request
         const payFastUrl = generatePayFastUrl(orderId, amount);
@@ -20,34 +20,4 @@ router.post('/payfast', async (req, res) => {
     }
 });
 
-<<<<<<< HEAD
-// Define additional payment routes as needed
-// payment.js
-// Endpoint to handle payment notifications/callbacks
-router.post('/notify',  res) => { 
-	console.log(req.body);
-    // Extract parameters from the request body
-    const {
-        m_payment_id,
-        pf_payment_id,
-        payment_status,
-        amount_gross,
-        amount_fee,
-        amount_net,
-        name_first,
-        name_last,
-        email_address,
-        signature
-    } = req.body;
-
-    // Verify signature (if required)
-    // Perform necessary processing based on payment status
-    // Update your database with payment information
-    
-    // Respond to the payment gateway with a success message
-    res.status(200).send('Payment notification received');
-});
-
-=======
->>>>>>> 4140981d3a6bfbc87723a4a1c4167a51a93ec329
 module.exports = router;
