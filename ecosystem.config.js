da6ac96d827895160fecfca03941d46c86057f2e
Itumeleng Malgas <aaron.tumi@live.co.za>
2024-03-12 23:08:48 +0200

    add pm2

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,16 +1,35 @@
 module.exports = {
-  apps : [{
-    script: 'cd frontend && npm start',
-  }],
+  apps : [
+    {
+      name: 'frontend',
+      script: 'cd frontend && npm start',
+    },
+    {
+      name: 'backend',
+      script: 'cd backend && npm start',
+    }
+],
 
   deploy : {
-    production : {
+    production_frontend : {
       user : 'ubuntu',
       key: '~/.ssh/school',
       host : '54.152.74.112',
       ref  : 'origin/main',
       repo : 'git@github.com:Yandah1/Noble-perfumes.git',
-      path : '/home/ubuntu',
+      path : '/home/ubuntu/frontend',
+      'pre-deploy-local': '',
+      'post-deploy' : 'source ~/.nvm/nvm.sh && cd frontend && npm install && npm run build && pm2 reload ecosystem.config.js --env production',
+      'pre-setup': '',
+      'ssh_options': 'ForwardAgent=yes'
+    },
+    production_backend : {
+      user : 'ubuntu',
+      key: '~/.ssh/school',
+      host : '34.204.81.17',
+      ref  : 'origin/main',
+      repo : 'git@github.com:Yandah1/Noble-perfumes.git',
+      path : '/home/ubuntu/backend',
       'pre-deploy-local': '',
       'post-deploy' : 'source ~/.nvm/nvm.sh && cd frontend && npm install && npm run build && pm2 reload ecosystem.config.js --env production',
       'pre-setup': '',
