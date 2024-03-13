854bd56d7740beaed685bc1943bf08dc931f53a5
Itumeleng Malgas <aaron.tumi@live.co.za>
2024-03-13 14:03:17 +0200

    Only frontend

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -4,16 +4,9 @@ module.exports = {
       name: 'frontend',
       script: 'npm',
       args: 'start',
-      cwd: '~/source/frontend',
-    },
-    {
-      name: 'backend',
-      script: 'npm',
-      args: 'start',
-      cwd: '~/source/backend',
-    },
-],
-
+      cwd: '~/current/frontend',
+    }],
+    
   deploy : {
     production_frontend : {
       user : 'ubuntu',
@@ -24,18 +17,6 @@ module.exports = {
       path : '/home/ubuntu/',
       'pre-deploy-local': '',
       'post-deploy' : 'source ~/.nvm/nvm.sh && cd frontend && npm install && npm run build && cd .. && pm2 reload ecosystem.config.js --env production_frontend',
-      'pre-setup': '',
-      'ssh_options': 'ForwardAgent=yes'
-    },
-    production_backend : {
-      user : 'ubuntu',
-      key: '~/.ssh/school',
-      host : '34.204.81.17',
-      ref  : 'origin/main',
-      repo : 'git@github.com:Yandah1/Noble-perfumes.git',
-      path : '/home/ubuntu/backend',
-      'pre-deploy-local': '',
-      'post-deploy' : 'source ~/.nvm/nvm.sh && cd frontend && npm install && npm run build && cd .. && pm2 reload ecosystem.config.js --env production_backend',
       'pre-setup': '',
       'ssh_options': 'ForwardAgent=yes'
     }
