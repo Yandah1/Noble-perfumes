0c9bc770742174c8d09ca375a6f934470e99e344
Yandah1 <nonku003@gmail.com>
2024-03-16 23:07:49 +0200

    app.js updated

--- backend/app.js
+++ backend/app.js
@@ -45,7 +45,7 @@ const api = process.env.API_URL;
 app.use(`${api}/categories`, categoriesRoutes);
 app.use(`${api}/products`, productsRoutes);
 app.use(`${api}/users`, usersRoutes);
-app.use(`${api}/orders`, ordersRoutes);
+app.use(`${api}/orders`, ordersRoutes(io));
 app.use(`${api}/guest-checkouts`, guestCheckoutRoutes);
 app.use(`${api}/payments`, paymentsRoutes);
 app.use(`${api}/orderTracking`, orderTrackingRoutes);
