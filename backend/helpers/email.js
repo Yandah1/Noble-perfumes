9ddbca4b478f7b9b36c578080b449ae980ffc594
Yandah1 <nonku003@gmail.com>
2024-03-21 19:39:24 +0200

    notification error fixed

--- backend/helpers/email.js
+++ backend/helpers/email.js
@@ -28,7 +28,25 @@ async function sendConfirmationEmail(email, orderTrackingNumber) {
       throw error;
     }
   }
-  
-  module.exports = {
-    sendConfirmationEmail
-  };
+
+// Function to send the notification to the customer
+async function sendNotificationToCustomer(email, message) {
+  try {
+    const mailOptions = {
+      from: 'process.env.EMAIL_USER',
+      to: email,
+      subject: 'Payment Notification',
+      text: message
+    };
+
+    // Send the email
+    await transporter.sendMail(mailOptions);
+    console.log('Notification email sent to:', email);
+  } catch (error) {
+    console.error('Error sending notification email:', error);
+    throw error;
+  }
+}
+module.exports = {
+  sendConfirmationEmail
+};
